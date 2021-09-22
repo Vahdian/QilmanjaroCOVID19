7b381ac725e515ff76555a064145d69a620fa731
vahdian <germanpalero@gmail.com>
2021-09-23 01:50:26 +0200

    enviroment safe

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,13 @@
+import string
+import random
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
+import config
+
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = 'mongodb+srv://qilimanjaro:qilimanjaro@cluster0.ryhv1.mongodb.net/users?tls=true&tlsAllowInvalidCertificates=true'
+app.config["MONGO_URI"] = config.MONGO_URI
 mongo = PyMongo(app)
 
 
@@ -12,17 +16,19 @@ def create_user():
     username = request.json['username']
     email = request.json['email']
     password = request.json['password']
+    code = ''.join(random.SystemRandom().choice(
+        string.ascii_letters + string.digits) for _ in range(10))
 
     if username and email and password:
         hashed_password = generate_password_hash(password)
         id = mongo.db.users.insert(
             {'username': username, 'email': email,
-                'password': hashed_password}
+                'password': hashed_password, 'code': code}
         )
         response = {
             'id': str(id),
             'username': username, 'email': email,
-            'password': password,
+            'password': password, 'code': code
         }
         return response
     else:
